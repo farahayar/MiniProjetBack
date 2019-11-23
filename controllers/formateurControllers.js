4b5f8220970877ff1399f09f69ddfc312b152f3f
farahayar <ayari.farah98@gmail.com>
2019-11-23 19:43:04 +0100

    ajout fromation+ lister formation

--- controllers/formateurControllers.js
+++ controllers/formateurControllers.js
@@ -7,6 +7,7 @@ const multipart = require('connect-multiparty');
 const { mongooose } = require('./../db/config');
 
 const { Formateur } = require('./../models/formateur');
+const { Formation } = require('./../models/formation');
 var app = express();
 app.use(bodyParser.json());
 
@@ -18,25 +19,25 @@ app.post("/connection", (req, res) => {
     let email = data._email;
     let Password = data._pwd;
     console.log(email);
-        console.log(Password);
+    console.log(Password);
 
-    
-    Formateur.findOne({ email:email }).then((resultat) => {
+
+    Formateur.findOne({ email: email }).then((resultat) => {
 
 
         if (!resultat) {
-            
+
             res.status(400).send({ message: "email  incorrect" })
         }
-       
-        
-        if (!(Password===resultat.pwd)) {
+
+
+        if (!(Password === resultat.pwd)) {
 
             res.status(404).send({ message: "mot de passe incorrect" })
         }
-        console.log(" Formateur.admin"+ resultat.admin);
-        
-        let token = jwt.sign({ id: resultat._id, nom: resultat.nom , admin: resultat.admin }, "k").toString();
+        console.log(" Formateur.admin" + resultat.admin);
+
+        let token = jwt.sign({ id: resultat._id, nom: resultat.nom, admin: resultat.admin }, "k").toString();
         res.status(200).send({ token });
 
     }).catch((err) => {
@@ -76,7 +77,7 @@ app.post("/inscription", (req, res) => {
 
 });
 
-app.get('/Lister',(req,res)=>{
+app.get('/Lister', (req, res) => {
     Formateur.find().then((formateurs) => {
         if (formateurs) {
             res.status(200).send(formateurs);
@@ -86,38 +87,122 @@ app.get('/Lister',(req,res)=>{
     })
 });
 
-app.post("/ajoutFormateur",multipartMiddleware,(req,res)=>{
-    let data=JSON.parse(req.body.formateur);
-    let image=req.files.image;
-    
-    let ext=image.type.split('/')[1];
-    let imagePath="assets/" + data._nom + "." +ext;
-    console.log(imagePath);
-    
-    fs.renameSync(req.files.image.path, imagePath);
-    let im ="http://localhost:3000/"+ data._nom+ "." +ext;
+app.post("/ajoutFormateur", multipartMiddleware, (req, res) => {
+    let data = JSON.parse(req.body.formateur);
+    let image = req.files.image;
 
-    let formateur=new Formateur({
-        nom :data._nom,
-        prenom:data._prenom,
-        age:data._age,
-        fonction:data._fonction,
-        tel:data._numero_tel,
-        email:data._email,
-        salaire:data._salaire,
-        image_formateur:im
+    let ext = image.type.split('/')[1];
+    let imagePath = "assets/" + data._nom + "." + ext;
+    console.log(imagePath);
+
+    fs.renameSync(req.files.image.path, imagePath);
+    let im = "http://localhost:3000/" + data._nom + "." + ext;
+
+    let formateur = new Formateur({
+        nom: data._nom,
+        prenom: data._prenom,
+        age: data._age,
+        fonction: data._fonction,
+        numero_tel: data._numero_tel,
+        email: data._email,
+        salaire: data._salaire,
+        image_formateur: im
     })
 
-    formateur.save().then(()=>{
+    formateur.save().then(() => {
         res.status(200).send(formateur);
-    }).catch((err)=>{
+    }).catch((err) => {
         res.status(400).send({
-            message:"erreur : "+err
-            
+            message: "erreur : " + err
+
         })
         console.log(err);
     })
+});
+
+app.put('/consulterFormateur/:id', (req, res) => {
+
+    let id = req.params.id;
+
+    Formateur.findById({ _id: id }).then((format) => {
+
+
+        res.status(200).send(format);
+    }).catch((e) => {
+        res.status(400).send({
+            message: "erreur : " + e
+        })
+
+
+    });
 })
+
+app.delete('/supprimerFormateur/:email', (req, res) => {
+
+    let email = req.params.email;
+    console.log(email);
+
+    Formateur.findOneAndRemove({ email: email },
+
+        (err, doc) => {
+            if (!err) {
+                res.status(200).send(doc);
+                console.log(doc);
+            }
+            else { console.log('Error in Employee Delete :' + err) }
+        });
+});
+
+
+
+
+app.post("/ajouterformation", multipartMiddleware, (req, res) => {
+    console.log("aaaaaa");
+    
+    let data = JSON.parse(req.body.formation);
+    let image = req.files.image;
+    
+    let ext = image.type.split('/')[1];
+    let imagePath = "assets/" + data._titre + "." + ext;
+    console.log(imagePath);
+
+    fs.renameSync(req.files.image.path, imagePath);
+    let im = "http://localhost:3000/" + data._titre + "." + ext;
+
+    let formation = new Formation({
+        titre: data._titre,
+        description: data._description,
+        volume_horaire: data._volume_horaire,
+        prix: data._prix,
+        idFormateur: data._idformateur,
+        image_Formation: im
+
+    });
+
+    formation.save().then(() => {
+
+        res.status(200).send(formation);
+
+    }).catch((err) => {
+        res.status(400).send({
+            message: "erreur : " + err
+        })
+    })
+});
+
+app.get('/ListerFormations', (req, res) => {
+
+    Formation.find().then((formations) => {
+        if (formations) {
+            res.status(200).send(formations);
+        }
+        else { console.log("not found" + err.message) }
+
+    })
+
+});
+
+
 
 module.exports = app;
 
---
+++ models/formation.js
@@ -0,0 +1,48 @@
+const mongooose = require('mongoose');
+const validator = require('validator');
+
+const formationSchema = new mongooose.Schema(
+    {
+        titre: {
+            type: String,
+            require: true,
+            trim: true,
+            minlength: 2
+
+        },
+        description: {
+            type: String,
+            require: true,
+            trim: true,
+            minlength: 3,
+
+        },
+        volume_horaire: {
+            type: Number,
+            require: true,
+            trim: true
+        },
+        prix: {
+            type: String,
+            trim: true,
+            minlength: 3,
+
+        },
+        idformateur: {
+            type: String,
+            require: true,
+            trim: true,
+            minlength: 8,
+
+        }
+        , image_Formation: {
+            type: String,
+            require: true,
+            trim: true,
+        }
+    }
+
+);
+
+Formation = mongooose.model('Formation', formationSchema);
+module.exports = { Formation };
